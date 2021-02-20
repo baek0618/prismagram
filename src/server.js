df5034143df336a5f66ba9c63bd41bee4eca4b49
khbaek <khbaek@jlk-inspection.com>
2021-02-20 17:31:24 +0900

    prisma ..

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
-import  {GraphQLServer} from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+
 
 const PORT = process.env.PORT || 4000;
 // PORT 번호가 없으면 4000;
-
-const typeDefs = `
-  type Query{
-    hello: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-}
-
 const server = new GraphQLServer({
-  typeDefs, resolvers
-})
+  schema
+});
+
+
+server.express.use(logger("dev"));
 
 server.start({port: PORT}, ()=> console.log(`Server running on port http://localhost:${PORT}`))
